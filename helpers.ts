b8f681e1d08b9408a43114cbb592e33dd82a5e7e
TJ Epperson <tj.epperson070@gmail.com>
2023-05-03 15:41:33 +0900

    Implement middleware handling

--- helpers.ts
+++ helpers.ts
@@ -3,6 +3,14 @@ import { PrimeRoute } from "./types/prime"
 export const handleRouting = (req: Request, primeRoutes: PrimeRoute[]) => {
   const url = new URL(req.url)
   for (const route of primeRoutes) {
+    // Apply Middleware
+    if (route.path === '*' && (route.method === '*' || route.method === req.method)) {
+      if (route.middleware) {
+        const middlewareResult = route.middleware(req)
+        if (middlewareResult instanceof Response) return middlewareResult
+        else continue
+      }
+    }
     if (!route.path.includes(':')) {
       if (route.path === url.pathname && route.method === req.method) {
         return route.handler(req)
